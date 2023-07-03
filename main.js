a15c87c50b60df0105794722eb63e70c31108a10
Abdoalwkel <alwkel138@gmail.com>
2023-07-03 22:38:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let photo = document.querySelector('.photo');
+let container = document.querySelector('.container-fluid');
+function phones(phone){
+
+    photo.src = phone;
+
+}
+function colors(color){
+
+    container.style.background =color;
+
+}
